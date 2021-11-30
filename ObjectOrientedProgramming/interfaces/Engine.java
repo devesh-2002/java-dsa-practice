ea360bfc3cfcffed7cb9db331afb11ba90e50e46
Devesh Rahatekar <79015420+devesh-2002@users.noreply.github.com>
2021-11-30 11:53:29 +0530

    Add files via upload

---
+++ ObjectOrientedProgramming/interfaces/Engine.java
@@ -0,0 +1,8 @@
+package com.ObjectOrientedProgramming.interfaces;
+
+public interface Engine {
+    static final int price = 78000; //You don't need to write static final actually
+    void start();
+    void stop();
+    void accelerate();
+}
