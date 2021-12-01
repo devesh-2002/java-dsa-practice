de8179f696347eb30995723ad230db749d278482
Devesh Rahatekar <79015420+devesh-2002@users.noreply.github.com>
2021-12-01 19:00:55 +0530

    Add files via upload

---
+++ ObjectOrientedProgramming/interfaces/CDPlayer.java
@@ -0,0 +1,13 @@
+package com.ObjectOrientedProgramming.interfaces;
+
+public class CDPlayer implements Media {
+    @Override
+    public void start() {
+        System.out.println("Start CD Player");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stop CD Player");
+    }
+}
