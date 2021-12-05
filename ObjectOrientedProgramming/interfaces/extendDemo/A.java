2807bf6b4483388c4527269ab087bf32c3d0f3ee
Devesh Rahatekar <79015420+devesh-2002@users.noreply.github.com>
2021-12-05 17:23:17 +0530

    Add files via upload

---
+++ ObjectOrientedProgramming/interfaces/extendDemo/A.java
@@ -0,0 +1,7 @@
+package com.ObjectOrientedProgramming.interfaces.extendDemo;
+
+public interface A {
+    static void greet(){ //static methods always require a body
+        System.out.println("Hello I am static method");
+    }
+}
