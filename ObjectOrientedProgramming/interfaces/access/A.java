1acfe98658001342134078c6fa6ae0c7454bf18a
Devesh Rahatekar <79015420+devesh-2002@users.noreply.github.com>
2021-12-13 08:11:49 +0530

    Create A.java

---
+++ ObjectOrientedProgramming/interfaces/access/A.java
@@ -0,0 +1,15 @@
+package com.ObjectOrientedProgramming.access;
+
+public class A {
+
+   protected int num;
+   private String name;
+    int[] arr;
+    public A(int num, String name) {
+        this.num = num;
+        this.name = name;
+        this.arr = new int[num];
+    }
+
+
+}
