7ee5ceed2683ba30696db6156999f0388c6c7eb9
Devesh Rahatekar <79015420+devesh-2002@users.noreply.github.com>
2021-12-14 08:53:57 +0530

    Add files via upload

---
+++ ObjectOrientedProgramming/interfaces/access/Main.java
@@ -0,0 +1,9 @@
+package com.ObjectOrientedProgramming.access;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(10,"Devesh");
+        //Access the data members, modify etc.
+
+    }
+}
