092b9b96efe9f3132997737ac3b0222d66ba19bc
Devesh Rahatekar <79015420+devesh-2002@users.noreply.github.com>
2021-12-25 08:05:19 +0530

    Add files via upload

---
+++ ObjectOrientedProgramming/interfaces/Polymorphism/Shapes.java
@@ -0,0 +1,9 @@
+package com.ObjectOrientedProgramming.Polymorphism;
+
+public class Shapes {
+ void area(){
+    System.out.println("I am in Shapes");
+}
+//If I just add static keyword, it would show error
+//static methods cannot be overridden
+}
